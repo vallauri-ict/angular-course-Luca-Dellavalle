19d294df19842abec42f29bc39a8c7d329aec52d
Luca-Dellavalle <62664083+Luca-Dellavalle@users.noreply.github.com>
2021-10-14 11:33:06 +0200

    Gestione dati con *ngFor e *ngIf

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent{
   title = 'Luca Dellavalle 5B INFO 2021/22';
+  
+  studentList = [
+    {name:'Ettore Esposito', hobby:'Karate', gender:'M', isPro: false},
+    {name:'Luca Pellissero',hobby:'paddle',gender:'M', isPro: false},
+    {name:'Luca Dellavalle',hobby:'volley',gender:'M', isPro: false},
+    {name:'Bianca Teleman', hobby:'Karate', gender:'F', isPro: false},
+    {name:'Giada Valinotto',hobby:'paddle',gender:'F', isPro: false},
+    {name:'Samuele De Toffol',hobby:'volley',gender:'M', isPro: false},
+    {name:'Gabriele Leone', hobby:'Karate', gender:'M', isPro: false},
+    {name:'Martina Velardi',hobby:'paddle',gender:'F', isPro: false},
+    {name:'Alberto Savoldelli',hobby:'volley',gender:'M', isPro: false},
+    {name:'Sara Pignata',hobby:'paddle',gender:'F', isPro: false},
+  ]
 }
--- my-app/src/app/student/student.component.ts
+++ my-app/src/app/student/student.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-student',
@@ -6,33 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-  studentList = [
-    {name:'Ettore Esposito', hobby:'Karate', gender:'M'},
-    {name:'Luca Pellissero',hobby:'paddle',gender:'M'},
-    {name:'Luca Dellavalle',hobby:'volley',gender:'M'},
-    {name:'Bianca Teleman', hobby:'Karate', gender:'F'},
-    {name:'Giada Valinotto',hobby:'paddle',gender:'F'},
-    {name:'Samuele De Toffol',hobby:'volley',gender:'M'},
-    {name:'Gabriele Leone', hobby:'Karate', gender:'M'},
-    {name:'Martina Velardi',hobby:'paddle',gender:'F'},
-    {name:'Alberto Savoldelli',hobby:'volley',gender:'M'},
-    {name:'Sara Pignata',hobby:'paddle',gender:'F'},
-  ]
-  student: any;
+  @Input() student:any ;
   
   constructor() { 
-    this.randomStudent();
+    
   }
 
   private randomStudent(){
-    let num = Math.floor(Math.random()*this.studentList.length);
-    this.student = this.studentList[num];
+    let num = Math.floor(Math.random() * 2);
+    if(num == 1){
+      this.student.isPro = true;
+    }
   }
 
   ngOnInit(): void {
+    this.randomStudent();
   }
   onStudentClick(){
-    this.randomStudent();
+    this.student.isPro = !this.student.isPro;
   }
 
 }
