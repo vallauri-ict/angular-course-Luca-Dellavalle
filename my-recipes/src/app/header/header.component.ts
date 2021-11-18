d64b031856335b6f9bb52e9dd1b5cacb1d258604
Luca-Dellavalle <62664083+Luca-Dellavalle@users.noreply.github.com>
2021-11-18 11:34:10 +0100

    visualizzare e nascondere ricette e lista della spesa tramite header

--- my-recipes/src/app/header/header.component.ts
+++ my-recipes/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() featureSelected = new EventEmitter<string>();
+
   collapsed:boolean = true;
   
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  OnSelect(feature:string){
+    this.featureSelected.emit(feature);
   }
 
 }
